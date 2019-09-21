1c5e574c36601c3dd8f75e4cdf57dd81843af09e
karan preet singh sasan <karanpreet_singh@intuit.com>
2019-09-21 20:58:22 +0530

    Correcting Code

--- src/org/sasanlabs/ciphers/stream/random/SimpleDegree3LFSR.java
+++ src/org/sasanlabs/ciphers/stream/random/SimpleDegree3LFSR.java
@@ -1,9 +1,12 @@
 package org.sasanlabs.ciphers.stream.random;
 
 /**
- * @author KSASAN preetkaran20@gmail.com 
+ * @author KSASAN preetkaran20@gmail.com
  * 
- * Implementation of Simple degree 3 Single Linear Feedback Shift Register
+ *         Implementation of Simple degree 3 Single Linear Feedback Shift
+ *         Register {@see <a
+ *         href=https://github.com/SasanLabs/SasanCiphers/blob/master/docs/SimpleDegree3LFSR.md>More
+ *         Information</a>}
  */
 public class SimpleDegree3LFSR {
 
@@ -14,9 +17,11 @@ public class SimpleDegree3LFSR {
 	public int getNextState() {
 		int value = initialState[initialState.length - 1];
 		int xorValue = 0;
-		for (int i = initialState.length - 1; i >= 1; i--) {
+		for (int i = initialState.length - 1; i >= 0; i--) {
 			xorValue = xorValue ^ (initialState[i] * multiplier[i]);
-			initialState[i] = initialState[i - 1];
+			if (i > 0) {
+				initialState[i] = initialState[i - 1];
+			}
 		}
 		initialState[0] = xorValue;
 		return value;
@@ -25,7 +30,7 @@ public class SimpleDegree3LFSR {
 	public static void main(String[] args) {
 		SimpleDegree3LFSR singleLFSR = new SimpleDegree3LFSR();
 		for (int i = 0; i < 200; i++) {
-			if (i % ((int)Math.pow(2, 3)-1) == 0) {
+			if (i % ((int) Math.pow(2, 3) - 1) == 0) {
 				System.out.println();
 			}
 			System.out.print(singleLFSR.getNextState());
