baf53cc00e61aea6f789d9a67bafc3f8d7d62015
karan preet singh sasan <karanpreet_singh@intuit.com>
2019-11-09 03:07:24 +0530

    DES initial commit

---
+++ src/org/sasanlabs/ciphers/block/des/ffunction/FFunction.java
@@ -0,0 +1,14 @@
+package org.sasanlabs.ciphers.block.des.ffunction;
+
+/**
+ * F-Function {@see <a href="https://github.com/SasanLabs/SasanCiphers/blob/master/docs/DES.md">More Information</a>}
+ * 
+ * @author preetkaran20@gmail.com KSASAN
+ */
+public class FFunction {
+	
+	//Expansion
+	// XOR
+	// S-Box
+	// P-Box
+}
