368dd96fd9c7341db3eb0a13e041150eccc9736a
karan preet singh sasan <karanpreet_singh@intuit.com>
2019-11-09 18:13:07 +0530

    Adding High Level Diagrams
    Signed-off-by: karan preet singh sasan <karanpreet_singh@intuit.com>

--- src/org/sasanlabs/ciphers/block/des/ffunction/FFunction.java
+++ src/org/sasanlabs/ciphers/block/des/ffunction/FFunction.java
@@ -1,13 +1,15 @@
 package org.sasanlabs.ciphers.block.des.ffunction;
 
 /**
- * F-Function {@see <a href="https://github.com/SasanLabs/SasanCiphers/blob/master/docs/DES.md">More Information</a>}
+ * F-Function {@see <a href=
+ * "https://github.com/SasanLabs/SasanCiphers/blob/master/docs/DES.md">More
+ * Information</a>}
  * 
  * @author preetkaran20@gmail.com KSASAN
  */
 public class FFunction {
-	
-	//Expansion
+
+	// Expansion
 	// XOR
 	// S-Box
 	// P-Box
